6e3560fda47aed391a0f048686c05869e7e285c2
Денис <denismalkin4@gmail.com>
2025-03-11 21:41:32 +0300

    check auto lint

---
+++ backend/src/trpc.ts
@@ -0,0 +1,39 @@
+import { initTRPC } from "@trpc/server";
+
+const memories = [
+  {
+    id: "head1",
+    name: "Header1",
+    description: "This is the header number 1",
+  },
+  {
+    id: "head2",
+    name: "Header2",
+    description: "This is the header number 2",
+  },
+  {
+    id: "head3",
+    name: "Header3",
+    description: "This is the header number 3",
+  },
+  {
+    id: "head4",
+    name: "Header4",
+    description: "This is the header number 4",
+  },
+  {
+    id: "head5",
+    name: "Header5",
+    description: "This is the header number 5",
+  },
+];
+
+const trpc = initTRPC.create();
+
+export const trpcRouter = trpc.router({
+  getMemories: trpc.procedure.query(() => {
+    return { memories };
+  }),
+});
+
+export type TrpcRouter = typeof trpcRouter;
